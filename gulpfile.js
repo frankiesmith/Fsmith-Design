277d4826151f035e8eb28205a5ec42a6a5c26c59
Frankie Smith <frankie@5hdagency.com>
2021-03-04 12:05:12 -0500

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,23 @@ const autoprefixer  = require('gulp-autoprefixer');
 const sourcemaps    = require('gulp-sourcemaps');
 
 const sassConfig = {
-	options: {
+  options: {
     includePaths: [
       'node_modules/foundation-sites/scss',
       'theme/scss'
     ],
-		outputStyle: 'compressed'
-	}
+    outputStyle: 'compressed'
+  }
+};
+
+const sassConfigBlocks = {
+  options: {
+    includePaths: [
+      'node_modules/foundation-sites/scss',
+      'theme/blocks'
+    ],
+    outputStyle: 'compressed'
+  }
 };
 
 gulp.task('sass', (cb) => {
@@ -20,10 +30,10 @@ gulp.task('sass', (cb) => {
       sourcemaps.init(),
       sass(sassConfig.options),
       autoprefixer({
-        browsers: ['last 2 versions', 'ie 10']
+        overrideBrowserslist: ['last 2 versions', 'ie 10']
       }),
       sourcemaps.write('.'),
-      gulp.dest('./theme')
+      gulp.dest('./theme'),
     ], cb);
 });
 
@@ -31,5 +41,31 @@ gulp.task('watch', () => {
   gulp.watch(['theme/theme.scss','theme/scss/*'], ['sass']);
 });
 
-gulp.task('default', ['watch','sass']);
+gulp.task('sassblocks', (cb) => {
+  pump([
+      gulp.src('./theme/scss/blocks.scss'),
+      sourcemaps.init(),
+      sass(sassConfigBlocks.options),
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions', 'ie 10']
+      }),
+      sourcemaps.write('.'),
+      gulp.dest('./theme/blocks'),
+    ], cb);
+});
+
+gulp.task('watchblocks', () => {
+  gulp.watch(['theme/scss/blocks.scss','theme/scss/blocks.scss/*'], ['sassblocks']);
+});
+
+
+
+gulp.task('default', ['watch','sass','watchblocks','sassblocks']);
+
 gulp.task('build', ['sass']);
+gulp.task('build', ['sassblocks']);
+
+
+
+
+
